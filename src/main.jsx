fd3bf8d4c2cffc21688e50842ec748f278e557a0
tal0311 <tal.amit0311@gmail.com>
2025-01-22 00:26:30 +0200

    tets

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import { Provider } from 'react-redux'
 import { store } from './store/store'
 import './index.css'
 
+
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
